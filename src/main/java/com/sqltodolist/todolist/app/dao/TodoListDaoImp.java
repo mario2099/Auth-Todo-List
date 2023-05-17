388e632d1351fab62c0f143bb51060e04c8170d1
Mario De Leon <mariodeleon3701@gmail.com>
2023-05-17 15:27:57 -0600

    Initial commit

---
+++ src/main/java/com/sqltodolist/todolist/app/dao/TodoListDaoImp.java
@@ -0,0 +1,43 @@
+package com.sqltodolist.todolist.app.dao;
+
+import com.sqltodolist.todolist.app.models.TodoList;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class TodoListDaoImp implements TodoListDao {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public List<TodoList> getTodos(){
+        String query = "FROM TodoList";
+        return entityManager.createQuery(query).getResultList();
+    };
+
+    @Override
+    public void addTodo(TodoList todoList){
+        entityManager.merge(todoList);
+    };
+
+    @Override
+    public void deleteTodo(Long id){
+        TodoList todoList = entityManager.find(TodoList.class, id);
+        entityManager.remove(todoList);
+    }
+    @Override
+    public void completeTodo(Long id){
+        TodoList todoList = entityManager.find(TodoList.class, id);
+        if (todoList != null) {
+            boolean currentCompleted = todoList.isCompleted();
+            todoList.setCompleted(!currentCompleted);
+            entityManager.merge(todoList);
+        }
+    }
+}
---
+++ src/main/java/com/sqltodolist/todolist/users/security/WebSecurityConfig.java
@@ -0,0 +1,4 @@
+package com.sqltodolist.todolist.users.security;
+
+public class WebSecurityConfig {
+}
